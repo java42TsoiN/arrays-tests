fad3859f57080875c8d1bab9eb5b8997dd060181
NTsoi <czoj.n95@mail.ru>
2021-10-13 20:56:22 +0300

    initial commit classwork #3

---
+++ arrays-tests/src/ArraysTests.java
@@ -0,0 +1,107 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+class ArraysTests {
+
+	@Test
+	void simpleArrayTests() {
+		int ar[] = new int[10];
+		ar[9]=2;
+		assertEquals(2,ar[9]);
+		int ar1[]=ar;
+		ar1[9]=20;
+		assertEquals(20,ar1[9]);
+		assertEquals(20,ar[9]);
+	}
+	@Test
+	void swapTests() {
+		int ar[]= {-10,20,12,40};
+		assertEquals(40, ar[3]);
+		assertEquals(12, ar[2]);
+		//swap
+		int tmp =ar[3];
+		ar[3]=ar[2];
+		ar[2]=tmp;
+		assertEquals(12, ar[3]);
+		assertEquals(40, ar[2]);
+	}
+	@Test
+	void twoDimensional1() {
+		int ar[][]= {{1,2,3},{10,20,30},{100,200,300}};
+		assertEquals(20,ar[1][1]);
+	}
+	@Test
+	void twoDimensional2() {
+		int ar1[]= {10,-3,8};
+		int ar2[]= {-7,11};
+		int ar3[]= {1,2,3,4};
+		int ar[][]= {ar1,ar2,ar3};
+		ar[1][1]=10;
+		assertEquals(10,ar2[1]);
+	}
+	@Test
+	void systemArrayCopy1() {
+		int ar[] = new int[5];
+		int ar1[] = {1,2,3};
+		int ar2[] = {4,5};
+		int expected[] = {1,2,3,4,5};
+		//TODO
+		System.arraycopy(ar1, 0, ar, 0,ar1.length);
+		System.arraycopy(ar2, 0, ar, ar1.length,ar2.length);
+		
+		assertArrayEquals(expected, ar);
+	}
+	@Test void systemArrayShift() {
+		int ar[] = {1,2,3,4,5};
+		//TODO using System.arraycopy
+		int expexted[] = {2,3,4,5,0};
+		System.arraycopy(ar, 1, ar, 0, ar.length-1);
+		ar[ar.length-1]=0;
+		assertArrayEquals(expexted,ar);
+	}
+	@Test
+	void systemArrayDelete() {
+		int ar[] = {1,2,3,4,5};
+		int actual[] = new int[4];
+		int expected[] = {1,2,4,5};
+		//TODO make sure the following assert will pass using System.arraycopy
+		System.arraycopy(ar, 0, actual, 0, 2);
+		System.arraycopy(ar, 3, actual, 2, 2);
+		assertArrayEquals(expected, actual);
+	}
+	@Test
+	void systemArrayInsert() {
+		int ar[] = {1,2,3,4,5};
+		int actual[] = new int[6];
+		int expected[] = {1,2,3,-10,4,5};
+		//TODO make sure the following assert will pass using System.arraycopy
+		
+		
+		//assertArrayEquals(expected, actual);
+	}
+	@Test
+	void arraysCopyOf() {
+		//TODO write tests for Arrays.copyOf functionality according to the doc
+//		Arrays.copyOf(null, 0);	
+	}
+	@Test 
+	void arrayCopyOfRange() {
+		//TODO write tests for Arrays.copyOfRange functionality according to the doc
+//		Arrays.copyOfRange(null, 0, 0)
+	}
+	@Test 
+	void arraysSort() {
+		//TODO write tests for Arrays.sort functionality according to the doc
+
+	}
+	@Test 
+	void arraysBinarySearch() {
+		//TODO write tests for Arrays.binarySearch functionality according to the doc
+
+	}
+	}
+
+
